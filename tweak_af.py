4285ef14d3395467f6f1a798afb3a3dcb3b389a6
Tomas Vymazal <tomas.vymazal@gmail.com>
2020-08-26 18:31:45 +0200

    code cleanup

--- tweak_af.py
+++ tweak_af.py
@@ -3,6 +3,7 @@
 import inspect
 
 tweak_dict = {}
+_call_dict = {}
 
 token_name = "_t" + "v" + "("
 token_length = len(token_name)
@@ -12,8 +13,7 @@ def reload_dict(call_filename):
     with open(call_filename, "r") as f:
         content = f.readlines()
 
-    # you may also want to remove whitespace characters like `\n` at the end of each line
-    content = [x.strip() for x in content]
+    content = [x.strip() for x in content]  # strip whitespaces
 
     line_counter = 0
 
@@ -43,18 +43,13 @@ def reload_dict(call_filename):
 
             found_on_line_counter += 1
 
-            call_hash = (line_counter+1) * 1000 + index
-
-            # print(f"_tv() found at {call_filename}:{call_hash}, {line}")
-            # print(f"_tv() found at {call_filename}:{line_counter+1}:{found_on_line_counter} == {value}")
-
             if call_filename not in tweak_dict:
                 tweak_dict[call_filename] = {}
 
-            if (line_counter+1) not in tweak_dict[call_filename]:
-                tweak_dict[call_filename][(line_counter+1)] = {}
+            if (line_counter + 1) not in tweak_dict[call_filename]:
+                tweak_dict[call_filename][(line_counter + 1)] = {}
 
-            tweak_dict[call_filename][(line_counter+1)][found_on_line_counter] = int(value)
+            tweak_dict[call_filename][(line_counter + 1)][found_on_line_counter] = int(value)
 
         line_counter += 1
 
@@ -62,13 +57,11 @@ def reload_dict(call_filename):
 def resolve_value(default_value, call_filename, line, line_order):
     reload_dict(call_filename)
 
-    if call_filename in tweak_dict and line in tweak_dict[call_filename] and line_order in tweak_dict[call_filename][line]:
+    if call_filename in tweak_dict and line in tweak_dict[call_filename] \
+            and line_order in tweak_dict[call_filename][line]:
         return tweak_dict[call_filename][line][line_order]
     else:
         return default_value
-
-
-_call_dict = {}
 
 
 def _get_order(line, inst):
@@ -80,14 +73,14 @@ def _get_order(line, inst):
 
     try:
         index = _call_dict[__file__][line].index(inst)
-    except:
+    except ValueError:
         index = -1
 
     if index == -1:
         _call_dict[__file__][line].append(inst)
         return len(_call_dict[__file__][line])
     else:
-        return index+1
+        return index + 1
 
 
 def _tv(default_value):
@@ -98,7 +91,6 @@ def _tv(default_value):
     # we need to identify each _tv() call within a file, we need them unique, otherwise, the user would have to
     # supply key name
 
-    call_hash = line * 1000 + inst    # probably not the best idea
     call_filename = __file__
 
     line_order = _get_order(line, inst)
@@ -109,21 +101,20 @@ def _tv(default_value):
 
 
 def test_func_inner():
-    print(f"inner: {_tv(6)}")
+    print(f"inner: {_tv(8)}")
 
 
 def test_func():
-    # a = _tv(00)
-    # b = _tv(10)
-    # c = _tv(2) + _tv(3)
+    a = _tv(00)
+    b = _tv(10)
+    c = _tv(2) + _tv(3)
 
-    # print(f"{a}, {b}")
+    print(f"{a}, {b}")
 
     while True:
-        print(_tv(3), _tv(4))
+        print(_tv(2), _tv(4))
         test_func_inner()
 
 
 if __name__ == '__main__':
-    print(__file__)
     test_func()
