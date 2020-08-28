847450a5c085423a130c04fd73d84dc9b596d5da
Tomas Vymazal <tomas.vymazal@gmail.com>
2020-08-28 11:23:57 +0200

    refactoring

--- test_tv.sh
+++ test_tv.sh
@@ -1,7 +1,7 @@
-cp tests/tv_test_og.py tv_test.py
+cp test_helpers/tv_test_og.py tv_test.py
 
 python3 tv_test.py &
 sleep 0.1
 
-cp tests/tv_test_hit.py tv_test.py
-cp tests/tv_test_og.py tv_test.py
+cp test_helpers/tv_test_hit.py tv_test.py
+cp test_helpers/tv_test_og.py tv_test.py
