8f506a54daea97e165a226b7d729d6a0a8b958ca
Tomas Vymazal <tomas.vymazal@gmail.com>
2020-09-14 21:15:44 +0200

    example made (even) more cool

--- example.py
+++ example.py
@@ -4,9 +4,15 @@
 from tweak_af import tv, tf  # , set_tweakables_enabled
 
 
+def func_called_from_tweakable():
+    return 42  # if you uncomment the call in test_func() during the runtime, try to change this function too
+
+
 # 3. try to change body of this function - while the original script is still running
+# Try to uncomment the part in the return statement (during the runtime), save it and see the change
 def test_func(a, b):
-    return str(a + b) + " " + "test"
+    return a + b  # + func_called_from_tweakable()
+
 
 def str_test_func(param):
     return "badass " + param
@@ -18,7 +24,7 @@ if __name__ == '__main__':
 
     while True:
         # 4. try to change the tweakable value (3) to something else - while the original script is still running
-        print(tf(lambda: test_func(1, tv(3))))
-        print(tf(lambda: str_test_func(tv("tomas"))))
+        print(tf(lambda: test_func(1, tv(3))))  # lambda is currently needed, because reasons :-)
+        print(tf(lambda: str_test_func(tv("original"))))
 
 # 5. you can break tv() logic by adding new lines above it, but tf() will work when adding new lines to functions
